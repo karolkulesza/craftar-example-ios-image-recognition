10aedfc00296a1ae6473827d4bcd43b223543d5d
Luis Martinell Andreu <luis@catchoom.com>
2014-08-06 17:52:37 +0200

    First commit for Image Recognition SDK Examples app.

---
+++ CatchoomSDK_Examples/Example 2 - Finder mode image Recognition/CloudRecognitionFinderModeViewController.m
@@ -0,0 +1,115 @@
+//
+//  CloudRecognitionSnapPhotoViewController.m
+//  catchoom-sdk-sampleapp
+//
+//  Created by Luis Martinell Andreu on 9/17/13.
+//  Copyright (c) 2013 Catchoom. All rights reserved.
+//
+
+#import "CloudRecognitionFinderModeViewController.h"
+#import <CatchoomSDK/CatchoomSDK.h>
+#import <CatchoomSDK/CatchoomCloudRecognitionItem.h>
+
+@interface CloudRecognitionFinderModeViewController () <CatchoomSDKProtocol, CatchoomCloudRecognitionProtocol> {
+    CatchoomSDK *_sdk;
+    CatchoomCloudRecognition *_crs;
+}
+
+@end
+
+@implementation CloudRecognitionFinderModeViewController
+
+#pragma mark view initialization
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // setup the Catchoom SDK
+    _sdk = [CatchoomSDK sharedCatchoomSDK];
+    _sdk.delegate = self;
+    
+    _crs = [_sdk getCloudRecognitionInterface];
+    _crs.delegate = self;
+    
+}
+
+- (void) viewWillAppear:(BOOL) animated {
+    [super viewWillAppear:animated];
+    // Start Video Preview for search and tracking
+    [_sdk startCaptureWithView:self._preview];
+}
+
+#pragma mark -
+
+
+#pragma mark Snap Photo mode implementation
+
+- (void) didStartCapture {
+    self._scanningOverlay.hidden = NO;
+    [self._scanningOverlay setNeedsDisplay];
+    [_crs startFinderMode];
+}
+
+
+- (void) didGetSearchResults:(NSArray *)resultItems {
+    self._scanningOverlay.hidden = YES;
+    [_crs stopFinderMode];
+    
+    if ([resultItems count] >= 1) {
+        // Found one item, launch its content on a webView:
+        CatchoomCloudRecognitionItem *item = [resultItems objectAtIndex:0];
+        
+        // Open URL in Webview
+        UIViewController *webViewController = [[UIViewController alloc] init];
+        UIWebView *uiWebView = [[UIWebView alloc] initWithFrame: self.view.frame];
+        [uiWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:item.url]]];
+        uiWebView.scalesPageToFit = YES;
+        [webViewController.view addSubview: uiWebView];
+        [self.navigationController pushViewController:webViewController animated:YES];
+        self._scanningOverlay.hidden = YES;
+    }
+    self._scanningOverlay.hidden = NO;
+    [self._scanningOverlay setNeedsDisplay];
+    [_crs startFinderMode];
+}
+
+
+- (void) didFailWithError:(CatchoomSDKError *)error {
+    self._scanningOverlay.hidden = NO;
+    [self._scanningOverlay setNeedsDisplay];
+    [_crs startFinderMode];
+}
+
+- (void) didValidateToken {
+    // Token valid, do nothing
+}
+
+#pragma mark -
+
+
+#pragma mark view lifecycle
+
+- (void) viewWillDisappear:(BOOL)animated {
+    [_sdk stopCapture];
+    [super viewWillDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+
+@end
---
+++ README.md
@@ -0,0 +1,44 @@
+## Catchoom CraftAR - iOS Image Recognition SDK examples
+
+### Introduction
+
+Catchoom allows to create recognition experiences using the Catchoom CraftAR Service and the Image Recognition SDK ('Mobile SDK').
+
+With Catchoom, you can create amazing apps that provide digital content
+for real-life objects like printed media, packaging among others. You
+can use our online web panel or APIs, to upload images to be recognized and set
+content to display upon recognition in your Catchoom-powered app.
+
+This document describes mainly the Examples of different uses of the Service and the SDK.
+General use of the SDK can be found in the [Documentation section of Catchoom website](http://catchoom.com/documentation/sdk/ios-image-recognition-sdk/). Complete SDK documentation of the classes can be found within the SDK distribution.
+
+### How to use the examples
+
+This repository comes with an xCode project of an iOS app with several
+examples that show how to use the SDK.
+
+To run the examples follow these steps:
+ 1.  Open the CatchoomSDK_Examples.xcodeproj project.
+ 2.  Integrate the CatchoomSDK into the xCode project (see [below](#step-by-step-guide)).
+ 3.  Select an iOS 6 or 7 device (notice that the project will not
+     compile for the simulator).
+ 4.  Hit the run button.
+
+### Add CatchoomSDK to the Example project
+
+#### Requirements
+
+To build the project or use the library, you will need XCode 5 or newer,
+and at least the iOS 7.0 library.
+
+#### Step-by-step guide
+1.  Download the [Catchoom Image Recognition SDK](http://catchoom.com/documentation/sdk/android-image-recognition-sdk/) for Android.
+2.  Unzip the package
+3.  Copy the libs folder into the root of the project.
+
+#### Step-by-step guide
+1.  Download the [Catchoom Image Recognition SDK](http://catchoom.com/documentation/sdk/ios-image-recognition-sdk/) for iOS.
+2.  Unzip the package
+3.  Drag the following files into the project:
+ * CatchoomSDK.framework
+ * Pods.framework
